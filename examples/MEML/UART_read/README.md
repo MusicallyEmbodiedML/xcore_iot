065956fa05153e36ea2c59c8e68f625f012a1be5
nrs23 <nrs23@sussex.ac.uk>
2024-07-17 16:44:34 +0100

    [CHANGE] Parse comman delimeted ints

--- examples/MEML/UART_read/README.md
+++ examples/MEML/UART_read/README.md
@@ -2,6 +2,15 @@
 
 This example application demonstrates UART reading on the XK-EVK-XU316 (no TX, no loopback, no other feature demos).
 
+The demo expects to receive a UART stream of data of format:
+```
+int,int,int\n
+e.g. 
+10234,23040,20525\n
+```
+
+The demo will parse the three ints and print them to debug.
+
 In order for the demo to work, you must connect Pin 36 on J10 to the UART TX of your tranmitting device.
 
 The demo is set up to read UART with the following configuration:
--- examples/MEML/UART_read/src/demos/uart_demo.c
+++ examples/MEML/UART_read/src/demos/uart_demo.c
@@ -5,6 +5,7 @@
 #include <platform.h>
 #include <xs1.h>
 #include <stdbool.h>
+#include <string.h>
 
 /* Platform headers */
 #include "soc.h"
@@ -15,11 +16,32 @@
 #include "app_conf.h"
 #include "app_demos.h"
 
+#define MESSAGE_SIZE 20
+char buffer[MESSAGE_SIZE];
+
 void uart_rx_demo(uart_rx_t* uart_rx_ctx)
 {
     debug_printf("Initialised UART RX\n");
+    int i = 0;
     while(1) {
         uint8_t rx = uart_rx(uart_rx_ctx);
         debug_printf("Received: %u\n", rx);
+        if (rx == 10) {
+            debug_printf("Buffer: %s\n", buffer);
+            char * p;
+            p = strtok(buffer, ",");
+            while (p != NULL) {
+                int coord = atoi(p);
+                debug_printf("Coordinate: %u\n", coord);
+                p = strtok(NULL, ",");
+            }
+            i = 0;
+        } else {
+            if (i >= MESSAGE_SIZE)
+                i = 0;
+            buffer[i] = (char)rx;
+            i++;
+        }
+
     }
 }
